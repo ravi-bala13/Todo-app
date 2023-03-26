c7bea4ba0d48e6aad6c25759d3316f8cab1921e0
BalaMurugan <bals161616@gmail.com>
2023-03-26 21:40:24 +0530

    backend connected

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,61 +1,111 @@
-import React, {useState} from 'react'
-import Todo from './Todo'
-import TodoForm from './TodoForm'
+import React, { useEffect, useState } from "react";
+import Todo from "./Todo";
+import TodoForm from "./TodoForm";
+import axios from "axios";
 
 function TodoList() {
-    const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
-    const addTodo = todo => {
-        console.log('todo:', todo)
-        if(!todo.text){
-            return
-        }
+  const backendUrl = "https://todo-app-backend-bbhw.onrender.com";
 
-        const newTodos = [...todos, todo]
-        
-        setTodos(newTodos)
-        console.log('...todos:', ...todos)
+  const fetchFromDB = () => {
+    try {
+      axios.get(`${backendUrl}/todos`).then((res) => {
+        console.log("res", res);
+        setTodos(res.data);
+      });
+    } catch (error) {
+      console.log("error:", error);
     }
+  };
 
-    const updateTodo = (todoId, newValue) => {
-        if(!newValue){
-            return
-        }
-
-        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item )))
+  const insertToDB = (todo) => {
+    try {
+      axios.post(`${backendUrl}/todos`, todo).then((res) => {
+        console.log("res", res);
+      });
+    } catch (error) {
+      console.log("error:", error);
     }
+  };
 
-    const removeTodo = (id) => {
-        const removeArr = [...todos].filter(todo => todo.id !== id) 
-
-        setTodos(removeArr)
+  const deleteFromDB = (id) => {
+    try {
+      axios.delete(`${backendUrl}/todos/${id}`).then((res) => {
+        console.log("res", res);
+      });
+    } catch (error) {
+      console.log("error:", error);
     }
+  };
 
-    const completeTodo = id => {
-        
-        let updatedTodos = todos.map( todo => {
-           if(todo.id === id){
-            todo.isComplete = !todo.isComplete
-        
-           } 
-           return todo
-        } )
-        setTodos(updatedTodos)
-        console.log('updatedTodos:', updatedTodos)
+  const updateToDB = (id, todo) => {
+    try {
+      axios.patch(`${backendUrl}/todos/${id}`, todo).then((res) => {
+        console.log("res", res);
+      });
+    } catch (error) {
+      console.log("error:", error);
     }
+  };
 
-    return (
-        <div>
-            <h1>What's the plan for today?</h1>
-            <TodoForm onSubmit={addTodo} />
-            <Todo
-            todos={todos}
-            completeTodo={completeTodo}
-            removeTodo={removeTodo}
-            updateTodo={updateTodo}
-            />
-        </div>
-    )
+  useEffect(() => {
+    fetchFromDB();
+  }, []);
+
+  const addTodo = (todo) => {
+    console.log("todo:", todo);
+    if (!todo.text) {
+      return;
+    }
+    insertToDB(todo);
+    const newTodos = [...todos, todo];
+
+    setTodos(newTodos);
+    console.log("...todos:", ...todos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    console.log("update id", todoId, newValue);
+    if (!newValue) {
+      return;
+    }
+    updateToDB(todoId, newValue);
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removeArr = [...todos].filter((todo) => todo.id !== id);
+    deleteFromDB(id);
+    setTodos(removeArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+        updateToDB(id, todo);
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+    console.log("updatedTodos:", updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>What's the plan for today?</h1>
+      <TodoForm onSubmit={addTodo} />
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </div>
+  );
 }
 
-export default TodoList
+export default TodoList;
